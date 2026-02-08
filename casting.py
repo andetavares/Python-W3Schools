e7eb16e495c7928fae10387a3a1b7185816abaab
Anderson Tavares <anderson.tavares.3699@gmail.com>
2026-02-08 19:08:45 -0300

    Passo 3

---
+++ casting.py
@@ -0,0 +1,15 @@
+a=int(1)
+b=int(2.8)
+c=int("3")
+
+d=float(1)
+e=float(2.8)
+f=float("3")
+g=float("4.2")
+
+l=str("s1")
+m=str(2)
+n=str(3.0)
+
+list=[a,b,c,d,e,f,g,l,m,n]
+print(list)
---
+++ tipos_dados1.py
@@ -0,0 +1,56 @@
+a = "Hello World"
+b = 20 
+c = 20.5
+d = 1j
+e = ["apple", "banana", "cheery"]
+f = ("apple", "banana", "cheery")
+g = range(6)
+h = {"name":"Jhon", "age": "36"}
+i = frozenset({"apple", "banana", "cheery"})
+j = True
+k = b"hello"
+l = bytearray(5)
+m = memoryview(bytes(5))
+n = None
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
+print(e)
+print(type(e))
+
+print(f)
+print(type(f))
+
+print(g)
+print(type(g))
+
+print(h)
+print(type(h))
+
+print(i)
+print(type(i))
+
+print(j)
+print(type(j))
+
+print(k)
+print(type(k))
+
+print(l)
+print(type(l))
+
+print(m)
+print(type(m))
+
+print(n)
+print(type(n))
